edee1298e049e5c0ac04dd3718982b24a8f5b97e
ivashmelev <shmelevivan20@gmail.com>
2019-03-22 00:25:00 +0300

    v0.0.2

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const request = require('request');
+const cheerio = require('cheerio');
 const fs = require('fs');
 
 let url = "http://nodejs.org/dist/index.json";
-
-// request(url, (err, res, body) => {
-//   if(err) throw err;
-
-//   console.log(res.statusCode);
-//   console.log(body);
-//   fs.writeFileSync('log.txt', body);
-// });
 
 let getSitePage = (arr) => {
   let nameFile = "";
@@ -17,9 +10,23 @@ let getSitePage = (arr) => {
   let sendRequest = (url) => {
     request(url, (err, res, body) => {
       console.log(url);
+
       if(err) throw err;
-      nameFile = url.split("//");
-      fs.writeFileSync(nameFile[1]+".html", body);
+      
+      let $ = cheerio.load(body);
+      let text = $("body").text();
+      arrData = text.split(' ');
+      // arrData = JSON.stringify(arrData);
+      for(let i in arrData){
+        if(arrData[i]=="" || arr[i]==" " || arrData[i]==null){
+          arrData.splice(i, 1);
+        }
+      }
+      console.log(arrData[1]);
+      // console.log(arrData);
+      fs.writeFileSync('log.txt', arrData);
+      
+     
     });
   }
 
@@ -32,6 +39,6 @@ let getSitePage = (arr) => {
 
 
 
-arr = ['https://www.1ditis.ru', 'https://yandex.ru', 'https://google.com', 'https://youtube.com'];
+arr = ["https://ria.ru/20190315/1551813506.html?in=t"];
 
 getSitePage(arr);
